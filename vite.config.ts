bd8bfaa5912baaa453f906156c6feb76fe6f6e55
cramanan <cramananjaonapro@gmail.com>
2026-02-15 00:11:43 +0100

    feat: added solid scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, import.meta.dirname);
 
   return {
     build: { outDir: "api/vite/build" },
-    clearScreen: false,
     server: {
       proxy: { "/api": { target: env.VITE_API_URL, changeOrigin: true } },
     },
+    clearScreen: false,
+    plugins: [solid()],
   };
 });
