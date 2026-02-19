0112f216aea9de5b818352bb8d8ec8df41f12be4
cramanan <cramananjaonapro@gmail.com>
2026-02-19 01:52:38 +0100

    feat: added tanstack solid-router scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig, loadEnv } from "vite";
 import solid from "vite-plugin-solid";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, import.meta.dirname);
 
   return {
     build: { outDir: "internal/vite/build" },
-    clearScreen: false,
     server: {
       proxy: { "/api": { target: env.VITE_API_URL, changeOrigin: true } },
     },
-    plugins: [solid()],
+    clearScreen: false,
+    plugins: [
+      tanstackRouter({
+        target: "solid",
+        autoCodeSplitting: true,
+      }),
+      solid(),
+    ],
   };
 });
